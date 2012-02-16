c5d2dd1ed894e56454fd5b08c4c7d349c0258369
Dimitrios Pritsos <dpritsos@extremepro.gr>
2012-02-16 23:12:13 +0200

    The Code is geting a mess - REFACTORING REFACTORING REFACTORING

--- src/experiments_with_tables.py
+++ src/experiments_with_tables.py
@@ -6,14 +6,18 @@ sys.path.append('../../synergeticprocessing/src')
 sys.path.append('../../html2vectors/src')
 import numpy as np
 import tables as tb
-import html2tf.tables.cngrams as cng_tb
-import html2tf.tables.tbtools as tbtls
-from html2tf.dictionaries.tfdtools import TFDictHandler
+#import html2tf.tables.cngrams as cng_tb
+import html2vect.tables.tbtools as tbtls
+from html2vect.dict.tfdtools import TFDictHandler
+import sklearn.decomposition as decomp
 import sklearn.svm as svm
 import sklearn.svm.sparse as sp_svm
-import sklearn.linear_model as linear_model 
+import sklearn.linear_model as linear_model
+import sklearn.covariance as skcov
+from sklearn.metrics import precision_score, recall_score
 import scipy.sparse as sps
-from trainevaloneclssvm import SVMTE 
+from trainevaloneclssvm import SVMTE
+from sklearn.feature_extraction.text import TfidfTransformer
 
 
 class ResultsTable_desc(tb.IsDescription):
@@ -23,6 +27,10 @@ class ResultsTable_desc(tb.IsDescription):
     F1 = tb.Float32Col(pos=4)
     P = tb.Float32Col(pos=5)
     R = tb.Float32Col(pos=6)
+    TP = tb.Float32Col(pos=7)
+    FP = tb.Float32Col(pos=8)
+    TN = tb.Float32Col(pos=9)
+    FN = tb.Float32Col(pos=10)
     
 class MultiResultsTable_desc(tb.IsDescription):
     kfold = tb.UInt32Col(pos=1)
@@ -201,7 +209,7 @@ class CSVM_CrossVal(tbtls.TFTablesHandler):
                     #        train_X = np.where( training_earr_X[(j*chnk):((j+1)*chnk), 0:feat_len] > 0, training_earr_X[(j*chnk):((j+1)*chnk), 0:feat_len], 0)
                     #        train_X[ np.nonzero(train_X) ] = 1
                     #        csvm.partial_fit(train_X, training_earr_Y[(j*chnk):((j+1)*chnk)])
-                    ####################3
+                    ########################
                     print "Predict for kfold:k",k
                     #crossval_X = crossval_earr_X[:, 0:feat_len] 
                     crossval_Y = crossval_earr_Y[:]
@@ -263,17 +271,19 @@ class OCSVM_CrossVal(tbtls.TFTablesHandler):
             if (k+1) == kfolds:
                 end = end + self.kfold_mod[gnr]
             
-            training_pg_lst = list()
-            training_clss_tag_lst = list()
-            crossval_pg_lst = list()
-            crossval_clss_tag_lst = list()
-            
             for g in self.genres_lst:
+                training_pg_lst = list()
+                training_clss_tag_lst = list()
+                crossval_pg_lst = list()
+                crossval_clss_tag_lst = list()
+                
                 page_lst_tb = self.page_lst_tb[g].read()
+                
                 #Get the Training set for this Genre g
                 grn_trn_pg_lst = self.complementof_list( page_lst_tb['table_name'], start, end )
                 training_pg_lst.extend( grn_trn_pg_lst )
                 training_clss_tag_lst.extend( [self.gnr2clss[g]]*len(grn_trn_pg_lst) )
+                
                 #Get the Evaluation set for this Genre and later concatenate it to the rest of the Evaluation Genres
                 grn_page_lst_tb = page_lst_tb['table_name'][start:end]
                 crossval_pg_lst.extend( page_lst_tb['table_name'][start:end] )
@@ -295,7 +305,7 @@ class OCSVM_CrossVal(tbtls.TFTablesHandler):
                 print "Creating Dictionary - Sorted by Frequency - for ", g 
                 term_idx_d, kfold_Dictionary_TF_arr = self.TFTables2TFDict_n_TFArr(self.corpus_grp + self.trms_type_grp,\
                                                                                    training_pg_lst,\
-                                                                                   data_type=tbtls.default_TF_3grams_dtype)
+                                                                                   data_type=np.dtype([('terms', 'S128'), ('freq', 'float32')]))
                 
                 #print term_idx_d.items()[0:10]
                 #print kfold_Dictionary_TF_arr[0:10] 
@@ -374,26 +384,51 @@ class OCSVM_CrossVal(tbtls.TFTablesHandler):
                     print "Features Size:", feat_len      
                     for n in nu_lst:
                         ocsvm = svm.OneClassSVM(nu=n, kernel='rbf')
+                        #tfidf = TfidfTransformer()
+                        #ppca = decomp.ProbabilisticPCA(n_components=5)
+                        #ecov = skcov.EmpiricalCovariance() 
+                        #sgd = linear_model.SGDClassifier(n_iter=50, alpha=1e-5, n_jobs=1)
                         print "FIT model"
-                        #train_X = training_earr_X[:, 0:feat_len] 
+                        #train_X = training_earr_X[0:180, 0:feat_len] 
                         ## No Required for this implementation train_Y = training_earr_Y[:]
                         train_X = np.where( training_earr_X[:, 0:feat_len] > 0, training_earr_X[:, 0:feat_len], 0)
-                        train_X[ np.nonzero(train_X) ] = 1
-                        print train_X  
+                        #train_X = tfidf.fit_transform(train_X)
+                        #print train_X.todense()
+                        #train_X[ np.nonzero(train_X) ] = 1  
+                        train_X = np.divide(train_X, np.amax( train_X, axis=1 ).reshape(len(train_X), 1) )
+                        #print len(train_X), len(train_Y)
+                        #print train_X, train_Y
+                        #0/0
+                        #sgd.fit(train_X, train_Y)
+                        #ecov.fit(train_X)
+                        #ppca.fit(train_X)  
                         ocsvm.fit(train_X) 
-                        print "Predict for kfold:k",k
+                        print "Predict for kfold:k", k
                         #crossval_X = crossval_earr_X[:, 0:feat_len] 
-                        ## No Required for this implementation crossval_Y = crossval_earr_Y[:]
-                        crossval_X = np.where( crossval_earr_X[:, 0:feat_len] > 0, crossval_earr_X[:, 0:feat_len], 0)
-                        crossval_X[ np.nonzero(crossval_X) ] = 1
-                        res = ocsvm.predict(crossval_X[0:self.ocsvm_gnr_end_bound_dct[g]])
-                        tp = len( np.where( res == 1 )[0] )
-                        fn = len( np.where( res == -1 )[0] )
+                        #print len(crossval_X) 
                         
-                        res = ocsvm.predict(crossval_X[self.ocsvm_gnr_end_bound_dct[g]::])
-                        print res
-                        tn = len( np.where( res == -1 )[0] ) 
-                        fp = len( np.where( res == 1 )[0] )
+                        ## No Required for this implementation crossval_Y = crossval_earr_Y[:]
+                        crossval_Y = crossval_earr_Y[:]
+                        crossval_Y[0:self.ocsvm_gnr_end_bound_dct[g]] = 1
+                        crossval_Y[self.ocsvm_gnr_end_bound_dct[g]::] = -1
+                        crossval_X = np.where( crossval_earr_X[:, 0:feat_len] > 0, crossval_earr_X[:, 0:feat_len], 0)
+                        #crossval_X = tfidf.fit_transform(crossval_X)
+                        #crossval_X[ np.nonzero(crossval_X) ] = 1
+                        crossval_X = np.divide( crossval_X,  np.amax( crossval_X, axis=1 ).reshape(len(crossval_X), 1) )
+                        #print crossval_X 
+                        res0 = ocsvm.predict(crossval_X[0:self.ocsvm_gnr_end_bound_dct[g]])
+                        #res = ppca.score(crossval_X[0:self.ocsvm_gnr_end_bound_dct[g]])
+                        #res = ecov.mahalanobis(crossval_X[0:self.ocsvm_gnr_end_bound_dct[g]])
+                        #res = sgd.predict(crossval_X[0:self.ocsvm_gnr_end_bound_dct[g]])
+            
+                        tp = len( np.where( res0 == 1 )[0] )
+                        fn = len( np.where( res0 == -1 )[0] )
+                        res1 = ocsvm.predict(crossval_X[self.ocsvm_gnr_end_bound_dct[g]::])
+                        #res = ppca.score(crossval_X[self.ocsvm_gnr_end_bound_dct[g]::])
+                        #res = ecov.mahalanobis(crossval_X[self.ocsvm_gnr_end_bound_dct[g]::])
+                        #res = sgd.predict(crossval_X[self.ocsvm_gnr_end_bound_dct[g]::])
+                        tn = len( np.where( res1 == -1 )[0] ) 
+                        fp = len( np.where( res1 == 1 )[0] )
                                                 
                         if (tp + fp) != 0.0:
                             P = np.float(tp) / np.float( tp + fp )
@@ -411,13 +446,19 @@ class OCSVM_CrossVal(tbtls.TFTablesHandler):
                             Acc = np.float(tp + tn) / np.float(tp + tn + fp + fn)
                         else:
                             Acc = 0.0
-                        print tn, fn, tp, fp,"|", P, R, F1  
+                        print tp, fp
+                        print fn, tn 
+                        print P, precision_score( crossval_Y, np.hstack((res0, res1)) ), R, recall_score( crossval_Y, np.hstack((res0, res1)) ), F1 
                         res_table.row['kfold'] = k
                         res_table.row['nu'] = n
                         res_table.row['feat_num'] = feat_len
                         res_table.row['F1'] = F1
                         res_table.row['P'] = P
                         res_table.row['R'] = R
+                        res_table.row['TP'] = tp
+                        res_table.row['FP'] = fp
+                        res_table.row['FN'] = fn
+                        res_table.row['TN'] = tn
                         res_table.row.append()  
                         
                 res_table.flush()
@@ -426,6 +467,7 @@ class OCSVM_CrossVal(tbtls.TFTablesHandler):
         #self.prepare_data(10, None)
         end_dct = {"blog":20, "eshop":20, "faq":20, "frontpage":20, "listing":20, "php":20, "spage":20}
         self.evaluate(10, [0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5, 0.6, 0.75, 0.80, 0.9, 0.95], [30, 300, 3000], end_dct)
+        #self.evaluate(10, [1], [30, 300, 3000], end_dct)
 
 
 if __name__=='__main__':
@@ -440,15 +482,16 @@ if __name__=='__main__':
     #crp_crssvl_res = tb.openFile('/home/dimitrios/Synergy-Crawler/Santinis_7-web_genre/CSVM_CrossVal_Results.h5', 'w')
     #crp_crssvl_res = tb.openFile('/home/dimitrios/Synergy-Crawler/Automated_Crawled_Corpus/CSVM_CrossVal_Results.h5', 'w')
     
-    crp_tftb_h5 = tb.openFile('/home/dimitrios/Synergy-Crawler/Santinis_7-web_genre/Santini_corpus.h5', 'r')
+    crp_tftb_h5 = tb.openFile('/home/dimitrios/Synergy-Crawler/Santinis_7-web_genre/Santini_corpus_w.h5', 'r')
     #crp_tftb_h5 = tb.openFile('/home/dimitrios/Synergy-Crawler/Automated_Crawled_Corpus/ACC.h5', 'r')
-    crp_crssvl_data = tb.openFile('/home/dimitrios/Synergy-Crawler/Santinis_7-web_genre/OCSVM_CrossVal_Data.h5', 'r')
+    crp_crssvl_data = tb.openFile('/home/dimitrios/Synergy-Crawler/Santinis_7-web_genre/OCSVM_CrossVal_Data_w.h5', 'r')
     #crp_crssvl_data = tb.openFile('/home/dimitrios/Synergy-Crawler/Automated_Crawled_Corpus/CSVM_CrossVal_Data.h5', 'r')
-    crp_crssvl_res = tb.openFile('/home/dimitrios/Synergy-Crawler/Santinis_7-web_genre/OCSVM_CrossVal_Results.h5', 'w')
+    crp_crssvl_res = tb.openFile('/home/dimitrios/Synergy-Crawler/Santinis_7-web_genre/OCSVM_CrossVal_Results_w.h5', 'w')
     #crp_crssvl_res = tb.openFile('/home/dimitrios/Synergy-Crawler/Automated_Crawled_Corpus/CSVM_CrossVal_Results.h5', 'w')
     #genres = [ "blog", "eshop", "faq", "frontpage", "listing", "php", "spage"]
     
-    ocsvm_crossval = OCSVM_CrossVal(crp_tftb_h5, crp_crssvl_data, crp_crssvl_res, "/Santini_corpus", "/trigrams/")
+    ocsvm_crossval = OCSVM_CrossVal(crp_tftb_h5, crp_crssvl_data, crp_crssvl_res, "/Santini_corpus", "/words/")
+    #ocsvm_crossval = OCSVM_CrossVal(crp_tftb_h5, crp_crssvl_data, crp_crssvl_res, "/Santini_corpus", "/trigrams/")
     #csvm_crossval = CSVM_CrossVal(crp_tftb_h5, crp_crssvl_data, crp_crssvl_res, "/Automated_Crawled_Corpus", "/trigrams/")
     ocsvm_crossval.exec_test()
     
--- src/temp2.py
+++ src/temp2.py
@@ -4,7 +4,7 @@ import numpy as np
 
 #[ "blog", "eshop", "faq", "frontpage", "listing", "php", "spage"]
 
-fh = tb.openFile('/home/dimitrios/Synergy-Crawler/Santinis_7-web_genre/OCSVM_CrossVal_Results.h5', 'r')
+fh = tb.openFile('/home/dimitrios/Synergy-Crawler/Santinis_7-web_genre/3grams-OCSVM_CrossVal_Results_NORMALIZED.h5', 'r')
 #fh = tb.openFile('/home/dimitrios/Synergy-Crawler/Automated_Crawled_Corpus/CSVM_CrossVal_Results.h5', 'r')
 
 
@@ -17,7 +17,7 @@ for tbpntr in fh.walkNodes('/', classname='Table'):
     print tbpntr.name
     for nu in [0.05, 0.1, 0.15, 0.2, 0.3, 0.4, 0.5, 0.6, 0.75, 0.80, 0.9, 0.95]:
         c = 0.0
-        for row in tb[ np.where( (tb['nu'] == nu) & (tb['feat_num'] == 3000)) ]:
+        for row in tb[ np.where( (tb['nu'] == nu) & (tb['feat_num'] == 30)) ]:
             #print row 
             c += row[3]
         print c/10.0, nu
