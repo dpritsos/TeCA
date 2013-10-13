dba6fd8f03b55873571907c401526a1b34f373d5
Dimitrios Pritsos <dpritsos@extremepro.gr>
2013-10-13 21:46:01 +0300

    RFSE with pyTables and Numpy.Sparce are stable. Experiment are correct. Ploting is fine. Some fixes to Document counts are required

--- src/ploting/curve_pr_rfse.py
+++ src/ploting/curve_pr_rfse.py
@@ -1,4 +1,3 @@
-
 "Precision Recall Curves"
 
 
@@ -37,7 +36,7 @@ def prcurve(res_h5file, kfolds, params_path, genre_tag=None, mark_thres=None):
             pre_y = res_h5file.getNode(params_path+'/KFold'+str(k), name='predicted_Y').read()
 
             #Use the Truth Table for converting the exp_y to Binary case and append for this fold    
-            EY_lst.append( np.where(exp_y == pre_y, 1, 0) ) 
+            EY_lst.append( np.where(exp_y == pre_y, 1, 0) )
 
     #Make Tables for Ensemble Algo
     PS = np.hstack(PS_lst)
@@ -74,6 +73,7 @@ def prcurve(res_h5file, kfolds, params_path, genre_tag=None, mark_thres=None):
 
 def zero_class_dist(res_h5file, kfolds, params_path, genre_tag=None):
     
+    EY_lst = list()
     PS_lst = list()
     PY_lst = list()
 
@@ -98,6 +98,9 @@ def zero_class_dist(res_h5file, kfolds, params_path, genre_tag=None):
             pred_scores = res_h5file.getNode(params_path+'/KFold'+str(k), name='predicted_scores' ).read()
             PS_lst.append( pred_scores )
 
+            exp_y = res_h5file.getNode(params_path+'/KFold'+str(k), name='expected_Y' ).read()
+            EY_lst.append( exp_y )
+
             pre_y = res_h5file.getNode(params_path+'/KFold'+str(k), name='predicted_Y' ).read()
 
             PY_lst.append(pre_y)
@@ -105,11 +108,135 @@ def zero_class_dist(res_h5file, kfolds, params_path, genre_tag=None):
     #Make Tables for Ensemble Algo
     PS = np.hstack(PS_lst)
     PY = np.hstack(PY_lst)
+    EY = np.hstack(EY_lst)
 
     #Short Results by Predicted Scores
-    inv_srd_idx = np.argsort(PS)[::-1]
+    inv_srd_idx = np.argsort(PS)[:]
     PY = PY[ inv_srd_idx ]
     
-    Zero_Dist = np.where(PY == 0)
+    Zero_Dist = np.where( (PY == EY[inv_srd_idx]) & (PY == 0) )
 
     return Zero_Dist
+
+
+
+def zclass_dist_per_class(res_h5file, kfolds, params_path, genre_tag=None):
+    
+    EY_lst = list()
+    PY_lst = list()
+
+    if genre_tag:
+        pass
+        """
+        for k in kfolds:
+
+            pc_per_iter = res_h5file.getNode(params_path+'/KFold'+str(k), name='predicted_classes_per_iter').read()
+            gnr_pred_cnt = np.where(pc_per_iter == genre_tag, 1, 0) 
+            
+            fold_ps = np.sum(gnr_pred_cnt, axis=0) / np.float(pc_per_iter.shape[0])   
+            PS_lst.append(fold_ps) 
+
+            pre_y = res_h5file.getNode(params_path+'/KFold'+str(k), name='predicted_Y' ).read()
+
+            PY_lst.append(pre_y)
+        """
+    else:
+
+        for k in kfolds:
+            
+            #pred_scores = res_h5file.getNode(params_path+'/KFold'+str(k), name='predicted_scores' ).read()
+            #PS_lst.append( pred_scores )
+            exp_y = res_h5file.getNode(params_path+'/KFold'+str(k), name='expected_Y' ).read()
+            EY_lst.append( exp_y )
+
+            pre_y = res_h5file.getNode(params_path+'/KFold'+str(k), name='predicted_Y' ).read()
+
+            PY_lst.append(pre_y)
+
+    #Make Tables for Ensemble Algo
+    EY = np.hstack(EY_lst)
+    PY = np.hstack(PY_lst)
+
+    #Short Results by Predicted Scores
+    inv_srd_idx = np.argsort(EY)[:]
+    
+
+    PY = PY[ inv_srd_idx ]
+
+    print PY
+
+    """
+    last = -1
+    for ii in PY[inv_srd_idx]:
+        if ii != last:
+            print ii
+            last = ii
+    """
+    
+    Zero_Dist = np.where( (PY != EY[inv_srd_idx]) & (PY == 0) )
+
+    return Zero_Dist
+
+
+
+def ZClass_DocSize(res_h5file, kfolds, params_path, genre_tag=None):
+    
+    EY_lst = list()
+    PY_lst = list()
+    DCL_lst = list()
+    PS_lst = list()
+
+    if genre_tag:
+        
+        pass
+        
+    else:
+
+        for k in kfolds:
+
+            pc_per_iter = res_h5file.getNode(params_path+'/KFold'+str(k), name='predicted_classes_per_iter').read()
+            gnr_pred_cnt = np.where(pc_per_iter == genre_tag, 1, 0) 
+            
+            fold_ps = np.sum(gnr_pred_cnt, axis=0) / np.float(pc_per_iter.shape[0])   
+            PS_lst.append(fold_ps) 
+            
+            #pred_scores = res_h5file.getNode(params_path+'/KFold'+str(k), name='predicted_scores' ).read()
+            #PS_lst.append( pred_scores )
+            prmpath = "/vocab_size100000/features_size500/Sigma05/Iterations10"
+            doc_len = res_h5file.getNode(prmpath+'/KFold'+str(0), name='docs_term_counts' ).read()
+            DCL_lst.append( doc_len )
+            
+            exp_y = res_h5file.getNode(params_path+'/KFold'+str(k), name='expected_Y' ).read()
+            EY_lst.append( exp_y )
+
+            pre_y = res_h5file.getNode(params_path+'/KFold'+str(k), name='predicted_Y' ).read()
+
+            PY_lst.append(pre_y)
+
+    #Make Tables for Ensemble Algo
+    EY = np.hstack(EY_lst)
+    PY = np.hstack(PY_lst)
+    DCL = np.hstack(DCL_lst)
+    PS = np.hstack(PS_lst)
+
+    #Short Results by Predicted Scores
+    inv_srd_idx = np.argsort(EY)[:]
+    
+
+    PY = PY[ inv_srd_idx ]
+    EY = EY[ inv_srd_idx ]
+
+    print PY
+
+    """
+    last = -1
+    for ii in PY[inv_srd_idx]:
+        if ii != last:
+            print ii
+            last = ii
+    """
+    
+    Y = DCL[ (PY == EY ) & (PY == 0) ]
+    X = range( Y.shape[0] )
+
+    return X, Y
--- src/ploting/plot_RFSE_Corpus.py
+++ src/ploting/plot_RFSE_Corpus.py
@@ -1,19 +1,19 @@
-
-
+#!/usr/bin/env python
 
 import tables as tb
 import matplotlib.pyplot as plt
-from curve_pr_rfse import zero_class_dist, prcurve
+import matplotlib.gridspec as gspec
+from curve_pr_rfse import zero_class_dist, zclass_dist_per_class, prcurve, ZClass_DocSize
 from sklearn import grid_search
 
 kfolds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 params_range = {
-    '1.vocab_size' : [100000],
-    '2.features_size' : [1000, 5000, 10000, 70000],
+    '1.vocab_size' : [100000], #[5000, 10000, 50000, 100000],
+    '2.features_size' : [500, 1000, 5000, 10000, 50000, 90000],
     #'3.Bagging' : [0.66],
-    '4.Iterations' : [100],
-    '3.Sigma' : [0.5],
+    '4.Iterations' : [100], #[10, 50, 100],
+    '3.Sigma' : [0.5, 0.7, 0.9]
 } 
 
 #res_h5file = tb.openFile('/home/dimitrios/Synergy-Crawler/KI-04/C-KI04_TT-Char4Grams-Koppels-Bagging_method_kfolds-10_GridSearch_TEST.h5', 'r')
@@ -22,41 +22,63 @@ params_range = {
 res_h5file = tb.openFile('/home/dimitrios/Synergy-Crawler/SANTINIS/SANTINIS_Char4Grams_RFSE_kf-10_SigmaT-[0.5]_TestOnly-spirit1000.h5', 'r')
 
 
+color_pallet2 = { 500:['k']*24, 1000:['r']*24, 5000:['g']*24, 10000:['b']*24, 50000:['y']*24, 90000:['m']*24 }
+color_pallet = { 5000:['k']*24, 10000:['r']*24, 50000:['g']*24, 100000:['b']*24 }
+#[, , , , ['c']*24, ['m']*24, ['y']*24]
+symbol = [ "^", "*", "x", "+", "*", "^", "x", "+", "^", "*", "x", "+", "*", "^", "x", "+", "^", "*", "x", "+", "*", "^", "x", "+" ]
+line_type = [ "--", "--", "--", "--", "-" , "-", "-", "-", "--", "--", "--", "--", "-" , "-", "-", "-", "--", "--", "--", "--", "-" , "-", "-", "-" ]
 
+plt.figure(num=None, figsize=(22, 12), dpi=80, facecolor='w', edgecolor='k')
 
-color = ['k', 'k', 'k', 'k', 'k', 'k', 'k', 'k' ]
-symbol = [ "^", "*", "x", "+", "*", "^", "x", "+" ]
-line_type = [ "--", "--", "--", "--", "-" , "-", "-", "-" ]
+gs = gspec.GridSpec(5, 3)
 
-plt.figure(num=None, figsize=(12, 8), dpi=80, facecolor='w', edgecolor='k')
-#plt.subplot(2, 1, 1)
+sub1 = plt.subplot(gs[:-1, 0])
+sub2 = plt.subplot(gs[:-1, 1])
+sub3 = plt.subplot(gs[:-1, 2])
+#sub3 = plt.ylim(ymin=0, ymax=12000)
+
 
 Zero_Dist_lst = list()
 
+plot_cnt = 0
+last_voc_size = '5000'
+
 for i, params in enumerate(grid_search.IterGrid(params_range)):
 
-    params_path = "/".join( [ key.split('.')[1] + str(value).replace('.','') for key, value in sorted( params.items() ) ] )
-    params_path = '/' + params_path
-    
-    X, Y, mark_X, mark_Y = prcurve(res_h5file, kfolds, params_path, genre_tag=None)
+    if params['1.vocab_size'] > params['2.features_size']:
 
-    Zero_Dist_lst.append( zero_class_dist(res_h5file, kfolds, params_path, genre_tag=None) )
+        params_path = "/".join( [ key.split('.')[1] + str(value).replace('.','') for key, value in sorted( params.items() ) ] )
+        params_path = '/' + params_path
+        
+        X, Y, mark_X, mark_Y = prcurve(res_h5file, kfolds, params_path, genre_tag=None)
 
-    plt.plot(X, Y, color[i] + symbol[i] + line_type[i], markeredgewidth=2, label="("+str(i+1)+") RFSE"+params_path  )
-    
-    if mark_X !=None:
-        plt.plot(mark_X, mark_Y, color[i] + symbol[i], markeredgewidth=15)
+        Zero_Dist_lst.append( zero_class_dist(res_h5file, kfolds, params_path, genre_tag=None) )
 
-    plt.title( " ".join(params_path.split('/')) )
-    plt.grid(True)    
-    plt.legend(loc=3)
+        X_len, Y_len = ZClass_DocSize(res_h5file, kfolds, params_path, genre_tag=None)
 
-    
-#plt.subplot(2, 1, 2)
-#plt.boxplot(Zero_Dist_lst)
+        #Zero_Dist_lst.append( zclass_dist_per_class(res_h5file, kfolds, params_path, genre_tag=None) )
+
+        #color = color_pallet[ params['1.vocab_size'] ]
+        color = color_pallet2[ params['2.features_size'] ]
+
+        plot_cnt += 1
+
+        sub1.plot(X, Y, symbol[i] + line_type[i] + color[i], linewidth=1, markeredgewidth=2, label="("+str(plot_cnt)+") RFSE"+params_path  )
+        
+        if mark_X !=None:
+            plt.plot(mark_X, mark_Y, color[i] + symbol[i], markeredgewidth=15)
+
+        sub2.plot(X_len, Y_len,  symbol[i] + color[i], linewidth=1, markeredgewidth=2)
+
+        sub1.grid(True)
+        sub2.grid(True)
+        sub1.legend(loc=3, bbox_to_anchor=(0.08, -0.4), ncol=2, fancybox=True, shadow=True)    
+       
+
+sub3.boxplot(Zero_Dist_lst)
+
+plt.savefig('/home/dimitrios/Desktop/Expected_ZClass.pdf')
 
 plt.show()
-                                                                           
-
-res_h5file.close()
-    
+                                                                         
+res_h5file.close()   
